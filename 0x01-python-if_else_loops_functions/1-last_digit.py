91488b16c1c58a66247349c00eabc300b7e0667e
root <root@DL>
2024-01-03 03:32:57 +0300

    Updated indentation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,4 +8,4 @@ elif number == 0:
     print(f'Last digit of {number} is {last_number} and is 0')
 else:
     print(f'Last digit of {number} is -{last_number} '
-            'and is less than 6 and not 0')
+    'and is less than 6 and not 0')
