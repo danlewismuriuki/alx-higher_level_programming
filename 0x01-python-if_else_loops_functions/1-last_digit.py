e6ac44a5a36c3ed218e8068de6e4ec0325709d7b
root <root@DL>
2024-01-03 03:22:51 +0300

    Added a spacing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 last_number = abs(number) % 10
+
 if number > 5:
     print(f'Last digit of {number} is {last_number} and is greater than 5')
 elif number == 0:
