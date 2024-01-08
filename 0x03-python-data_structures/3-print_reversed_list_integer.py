83a5a07d16bbe8cec3d24b4a7831e2dbc297400b
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-01-08 20:35:13 +0300

    Added control flow

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for num in my_list:
-        print("{:d}".format(num))
+    """print all integers in the list in reverse"""
+    if my_list:
+        my_list.reverse()
+        for num in my_list:
+            print("{:d}".format(num))
