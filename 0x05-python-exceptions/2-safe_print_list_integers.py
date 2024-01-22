5df0faab9fbebaf3520c10e32f6ea5d170201857
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-01-22 19:27:28 +0300

    Added indentation

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,12 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     # function that prints the first x elements of a list and only integers.
-
-        count = 0
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                count = count + 1
-            except (ValueError, TypeError):
-                continue
-        print()
-        return count
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count = count + 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return count
