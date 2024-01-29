32747d6d84584f3eb1558b9919b0885098fe3b07
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-01-29 19:05:15 +0300

    Updated area method to check if any of args equal to zero

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 # 1-rectangle.py
 """Write a class Rectangle that defines a rectangle """
 
@@ -63,6 +64,8 @@ class Rectangle:
 
     def area(self):
         """ Instance returns the area of the Rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return 0;
         return (self.__width * self.__height)
 
         """ Instance returns the perimeter of the Rectangle """
