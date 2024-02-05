48e12c1ea3263dd67bd82f9400a21d33400dcb23
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-02-06 00:30:31 +0300

    Added an initialization of list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,6 +9,10 @@ prints the sorted list
 
 class MyList(list):
     """ This class has a method which prints sorted elements """
+    def __init__(self):
+        """ Initialize the  list object """
+        super().__init__()
+
     def print_sorted(self):
-        sorted_list = sorted(self)
-        print(sorted_list)
+        """ print sorted list """
+        print(sorted(self))
