80062fb1795e7e01b7742af00d92d911cf74f4b9
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-02-06 16:50:00 +0300

    Removed files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 
 def read_file(filename=""):
     """ Reads a txt file and prints to stdout """
-    with open(filename, encoding="utf-8") as file:
-        print(file.read(), end="")
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
