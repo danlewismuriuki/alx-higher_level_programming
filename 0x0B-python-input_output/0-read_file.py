f7dc6c4c0fbe3d4393cb9db16308076a27010ed9
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-02-06 16:55:39 +0300

    Removed dir

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-def read_file(filename=""):
-    """ Reads a txt file and prints to stdout """
-    with open(filename, "r", encoding="utf-8") as f:
-        print(f.read(), end="")
