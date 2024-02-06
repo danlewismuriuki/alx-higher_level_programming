99a55ad72a50549bdfa164779247b0479ff2b8cb
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-02-06 22:33:41 +0300

    Added a function that returns the JSON representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# 3-to_json_string.py
+
+def to_json_string(my_obj):
+    """ function that returns the JSON representation of an object """
+    json.string = json.dumps(my_obj)
