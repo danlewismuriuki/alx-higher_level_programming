17bdc3b0c7d50a16433445e4bc00f46d90fb174d
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-02-06 22:11:41 +0300

    Updated utf

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 # 1-write_file.py
+""" Defining a file writing method """
+
 
 def write_file(filename="", text=""):
     """ write a string to a text filename """
-    with open(filename, "w", encoding="UTF8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
