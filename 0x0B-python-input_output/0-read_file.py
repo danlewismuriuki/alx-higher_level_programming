f486b44cfd93ba8edd420bb72f1e2a183d52f40d
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-02-06 16:26:17 +0300

    Added a function that reads a text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """ Reads a txt file and prints to stdout """
+    with open(filename, 'r', encoding="utf-8") as file:
+        print(file.read(), end="")
