0493afa162c5f87ebdc978a6d0e0c5c98eb115bf
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-02-08 01:59:19 +0300

    Added a class Rectangle that inherits

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,20 +2,7 @@
 
 # 9-rectangle.py
 """ Module 9-rectangle"""
-
-
-class BaseGeometry:
-    """A class with public instance methods area and integer_validator"""
-    def area(self):
-        """raises an exception when called"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates that value is an integer greater than 0"""
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
