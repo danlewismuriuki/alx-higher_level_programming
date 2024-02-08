a50044342852845fc30c555bf0882d2a3a4c3857
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-02-08 03:29:48 +0300

    Updated a size to private instance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -17,7 +17,7 @@ class Square(Rectangle):
         """
 
         self.integer_validator("size", size)
-        self.size = size
+        self.__size = size
         super().__init__(size, size)
 
     def area(self):
