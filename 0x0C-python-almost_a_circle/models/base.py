356dc1dc4a95b9773c4933e505c7208148c29491
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-02-08 15:21:27 +0300

    Removed blanklines

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,22 +4,20 @@
 Module containing the Base class
 """
 
+
 class Base:
     """ base class for alll obects in the project """
 
-
-    # private class attribute 
+    # private class attribute
     __nb_objects = 0
-
 
     def __init__(self, id=None):
         """constructor for the Base class """
 
-
-        #If id is provided, use it, otherwise, increment
-           # and use __nb_objects
+        # If id is provided, use it, otherwise, increment
+        # and use __nb_objects
         if id is not None:
             self.id = id
         else:
-           type(self).__nb_objects += 1
-           self.id = type(self).__nb_objects
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
