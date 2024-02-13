9a2808002c29f1b240481a41f8d4aaccb6c295ce
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-02-13 03:41:17 +0300

    Added tests for the rectangle

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,37 @@
+import unittest
+from models.rectangle import Rectangle
+
+class TestRectangle(unittest.TestCase):
+    def test_constructor_with_id(self):
+        """ Test constructor with id argument """
+        rect = Rectangle(10, 20, 5, 6, id=1)
+        self.assertEqual(rect.id, 1)
+        self.assertEqual(rect.width, 10)
+        self.assertEqual(rect.height, 20)
+        self.assertEqual(rect.x, 5)
+        self.assertEqual(rect.y, 6)
+
+    def test_constructor_without_id(self):
+        """ Test constructor without id argument """
+        rect = Rectangle(15, 25, 7, 8)
+        self.assertEqual(rect.width, 15)
+        self.assertEqual(rect.height, 25)
+        self.assertEqual(rect.x, 7)
+        self.assertEqual(rect.y, 8)
+
+    def test_getters_and_setters(self):
+        """ Test getters and setters """
+        rect = Rectangle(20, 30, 10, 12)
+
+        rect.width = 25
+        rect.height = 35
+        rect.x = 15
+        rect.y = 18
+
+        self.assertEqual(rect.width, 25)
+        self.assertEqual(rect.height, 35)
+        self.assertEqual(rect.x, 15)
+        self.assertEqual(rect.y, 18)
+
+  if __name__ == '__main__':
+      unittest.main()
