3ac0d9860383af049b9e3e7dcf37426fa7ff910f
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-03-13 21:48:57 +0300

    Added a script that lists all records of the table second_table

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 --  script that lists all records of the table second_table of the database
-SELECT `name`, `score`
+SELECT `score`, `name`
 FROM `second_table`
 WHERE `name` != ""
 ORDER BY `score` DESC;
