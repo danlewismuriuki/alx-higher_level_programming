6acc448e30fc2e807631fc7d3a13d40c8cdfb992
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-04-09 04:20:46 +0300

    Added a script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function factorial(n) {
+  // Base case: factorial of 0 is 1
+  if (isNaN(n)) {
+    return 1;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    // Recursive case: n * factorial(n - 1)
+    return n * factorial(n - 1);
+  }
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
+
