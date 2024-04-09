d664318795032591baf726554cc7b7689f95a40f
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-04-09 04:11:47 +0300

    Added a script that prints

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,18 +1,4 @@
 #!/usr/bin/node
 
-const argCount = process.argv.length;
-
-if (argCount === 2) {
-  console.log("No arguments passed");
-} else if (argCount === 3) {
-  console.log("Argument found");
-} else {
-  const number = +process.argv[3];
-
-  if (isNaN(number)) {
-    console.log("Not a number");
-  } else {
-    console.log(`My number: ${number}`);
-  }
-}
-
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
