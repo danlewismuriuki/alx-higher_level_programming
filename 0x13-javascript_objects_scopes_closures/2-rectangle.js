ff16b486a5f94e6db1592e3008e7d1e90f25b4b9
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-04-10 07:29:00 +0300

    Added  a class Rectangle that defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+    /* this.width = undefined; */
+    /* this.height = undefined; */
+
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
