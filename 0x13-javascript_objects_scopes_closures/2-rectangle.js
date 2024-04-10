fe396626d79390c29c38af124d6b9cbdde9583f8
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-04-10 08:06:49 +0300

    Updated a class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-    /* this.width = undefined; */
-    /* this.height = undefined; */
-
-      return {};
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
