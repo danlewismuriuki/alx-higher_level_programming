ffa753b7267520fff97f5b16595a2da406c6d028
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-04-13 02:57:19 +0300

    Added a LIMIT to my results

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -22,7 +22,7 @@ def city_list(mysql_username, mysql_password, database_name):
     cursor = connection.cursor()
 
 
-    query = ("SELECT DISTINCT * FROM states ORDER BY states.id ASC")
+    query = "SELECT DISTINCT * FROM states ORDER BY states.id ASC LIMIT 5"
 
     cursor.execute(query)
 
