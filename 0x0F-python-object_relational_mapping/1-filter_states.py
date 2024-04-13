af65971fb353467760961ee5e15fd98d74772cfc
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-04-13 05:01:49 +0300

    Added a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
+"""
+This module connects a python script to a databse
+"""
 
-import MySQLdb
 import sys
+import MySQLdb
 
 
-def my_city(mysql_username, mysql_password, database_name):
+def city_list(mysql_username, mysql_password, database_name):
+
     connection = MySQLdb.connect(
             user=mysql_username,
-            passwd=mysql_password,
             host="localhost",
+            passwd=mysql_password,
             port=3306,
             db=database_name
             )
 
+    # create a cursor object to interact with the database
     cursor = connection.cursor()
 
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;"
 
     cursor.execute(query)
 
@@ -27,10 +32,11 @@ def my_city(mysql_username, mysql_password, database_name):
     cursor.close()
     connection.close()
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    # Read the arguments from the cli
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    my_city(mysql_username, mysql_password, database_name)
+    city_list(mysql_username, mysql_password, database_name)
