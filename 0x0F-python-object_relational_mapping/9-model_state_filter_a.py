31ab27f1541d7f530eb3fb6d990a45e84846bc4d
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-04-15 00:55:28 +0300

    Added a script that lists all State objects that contain the letter a from the database

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,19 +8,37 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
+
+def list_states_containing_a(mysql_username, mysql_password, database_name):
+    # Create the engine to connect to the database
 
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True
+            f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost/'
+            f'{database_name}',
+            pool_pre_ping=True
     )
 
-    my_session_maker = sessionmaker(bind=engine)
-    my_session = my_session_maker()
+    # create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    for state in my_session.query(State).order_by(State.id):
-        if "a" in state.name:
-            print("{}: {}".format(state.id, state.name))
+    # Query the State objects that contain the letter 'a' in their name
+    states = session.query(State).filter(
+            State.name.like('%a%')
+            ).order_by(State.id).all()
 
-    my_session.close()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    # close the session
+    session.close()
+
+
+if __name__ == "__main__":
+    # get the arguments from the CLI
+
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+list_states_containing_a(mysql_username, mysql_password, database_name)
