5340d3e81c529b954423fe7d7f0637bd20c5f8d5
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-04-15 03:59:28 +0300

    Added a script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,27 +1,50 @@
 #!/usr/bin/python3
 
 """
-Module that connects python script to a database
+Script that deletes all State objects with a name containing
+the letter 'a' from the database hbtn_0e_6_usa.
 """
+
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
 
+def delete_states_with_a(mysql_username, mysql_password, database_name):
+    """ Deletes all states with a name containing the letter 'a' """
+
+    # Create a connection to the database
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2], argv[3]),
+        f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost/'
+        f'{database_name}',
         pool_pre_ping=True
     )
 
-    my_session_maker = sessionmaker(bind=engine)
-    my_session = my_session_maker()
+    # Create a session using the engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    states = my_session.query(State).filter(State.name.like('%a%')).all()
-    for state in states:
-        my_session.delete(state)
-    my_session.commit()
+    # Query all states with a name containing the letter 'a'
+    states_to_delete = session.query(State).filter(
+            State.name.like('%a%')).all()
 
-    my_session.close()
+    # Delete all states found
+    for state in states_to_delete:
+        session.delete(state)
+
+    # Commit the transaction
+    session.commit()
+
+    # Close the session
+    session.close()
+
+
+if __name__ == "__main__":
+    # Read command-line arguments
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+    # Delete all states with a name containing the letter 'a'
+    delete_states_with_a(mysql_username, mysql_password, database_name)
