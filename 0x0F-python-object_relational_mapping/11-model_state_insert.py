66c825531720c42799c0bcd974529ac1562fd420
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-04-15 03:27:53 +0300

    Added a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,22 +8,39 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
+
+def add_state_louisiana(mysql_username, mysql_password, database_name):
+    """ Adds the state object "Louisiana" to the database and prints its ID """
+    # Create a connection to the database
 
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True
-    )
+            f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost/'
+            f'{database_name}',
+            pool_pre_ping=True
+            )
+    # Create a session using the engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    my_session_maker = sessionmaker(bind=engine)
-    my_session = my_session_maker()
+    # Create a new State object with the name "Louisiana"
+    new_state = State(name="Louisiana")
 
-    Base.metadata.create_all(engine)
+    # Add the new state to the session and commit the transaction
+    session.add(new_state)
+    session.commit()
 
-    new_obj = State(name="Louisiana")
-    my_session.add(new_obj)
-    my_session.commit()
-    print(new_obj.id)
+    # Print the new state's ID after creation
+    print(new_state.id)
 
-    my_session.close()
+    # Close the session
+    session.close()
+
+
+if __name__ == "__main__":
+    # Read command-line arguments
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+    # Add the state "Louisiana" to the database and print the new state's ID
+    add_state_louisiana(mysql_username, mysql_password, database_name)
