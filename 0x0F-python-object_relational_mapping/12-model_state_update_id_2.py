7523719207258d34b8d72c06269143cd4524f459
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-04-15 03:51:48 +0300

    Added a script that changes the name of a State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,26 +1,49 @@
 #!/usr/bin/python3
 
 """
-Module that connects python script to a database
+Script that changes the name of the State object with id = 2
+from the database hbtn_0e_6_usa to "New Mexico".
 """
+
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
 
+def update_state_name(mysql_username, mysql_password, database_name):
+    """ Changes the name of the state with id = 2 to 'New Mexico' """
+
+    # Create a connection to the database
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2], argv[3]),
+        f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost/'
+        f'{database_name}',
         pool_pre_ping=True
     )
 
-    my_session_maker = sessionmaker(bind=engine)
-    my_session = my_session_maker()
+    # Create a session using the engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    state = my_session.query(State).filter_by(id=2).first()
-    state.name = "New Mexico"
-    my_session.commit()
+    # Query the state with id = 2
+    state_to_update = session.query(State).filter_by(id=2).first()
 
-    my_session.close()
+    # Check if the state with id = 2 exists
+    if state_to_update:
+        # Change the name of the state to 'New Mexico'
+        state_to_update.name = "New Mexico"
+        # Commit the transaction
+        session.commit()
+
+    # Close the session
+    session.close()
+
+
+if __name__ == "__main__":
+    # Read command-line arguments
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+    # Update the name of the state with id = 2 to 'New Mexico'
+    update_state_name(mysql_username, mysql_password, database_name)
