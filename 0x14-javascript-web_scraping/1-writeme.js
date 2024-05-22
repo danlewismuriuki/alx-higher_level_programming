25daf8e5c9e234d7fcf554b8ef80c54817444840
Dan <danlewismuriuki@gmail.com>
2024-05-22 17:37:48 +0300

    Updated the documentation

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 // write a string to a file
 
-/**
- * write a string to a file
- *
- * This script takes two args from the cmd line argument
- * It writed the provided string to the specified file
- *
- * @param {string} filepath to the file to write
- * @param {string} content - the string to write the file
- * @returns {void}
- */
+/// **
+// * write a string to a file
+// *
+// * This script takes two args from the cmd line argument
+// * It writed the provided string to the specified file
+// *
+// * @param {string} filepath to the file to write
+// * @param {string} content - the string to write the file
+// * @returns {void}
+// */
 
 // import fs module
 const fs = require('fs');
@@ -19,26 +19,26 @@ const fs = require('fs');
 const filepath = process.argv[2];
 
 // check if fil path is provided
-if (!filepath) {
-  console.error('Usage: node 1-writeme.js <filepath>');
-  process.exit(1);
-}
+// if (!filepath) {
+// console.error('Usage: node 1-writeme.js <filepath>');
+// process.exit(1);
+// }
 
 // Get the content to write from the cmd
 const content = process.argv[3];
 
 // check if the content is provided
-if (!content) {
-  // display message if content is absent
-  console.log('No content to print');
-  process.exit(1);
-}
+// if (!content) {
+// display message if content is absent
+// console.log('No content to print');
+// process.exit(1);
+// }
 
 // write contet to the specified file
 fs.writeFile(filepath, content, 'utf-8', err => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(`Content successfully written to ${filepath}`);
-  }
+  }// else {
+  // console.log(`Content successfully written to ${filepath}`);
+  // }
 });
