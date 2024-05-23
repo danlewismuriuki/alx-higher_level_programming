f92a2a450853359eb6d7e5276941ca8b99d22ab8
Dan <danlewismuriuki@gmail.com>
2024-05-23 03:24:10 +0300

    feat: add script to print characters of a Star Wars movie
    This script takes a Movie ID as input, fetches data from the Star Wars API
    for the specified movie, extracts the character URLs, fetches
    data for each character, and prints the names of all the
    characters in the movie, one per line.

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,43 +1,25 @@
 #!/usr/bin/node
-
-// import request
 const request = require('request');
 
-// Check if the Movie ID is provided as a command-line argument
-if (process.argv.length !== 3) {
-  console.error('Usage: ./101-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
-
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-// Make HTTP GET request to the Star Wars API
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  // Parse the JSON response
-  const movieData = JSON.parse(body);
-
-  // Extract character URLs from the movie data
-  const characterURLs = movieData.characters;
-
-  // Iterate through each character URL and make a request to fetch the character data
-  characterURLs.forEach(characterURL => {
-    request(characterURL, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-        return;
-      }
-
-      // Parse the character data
-      const characterData = JSON.parse(body);
-
-      // Print the character name
-      console.log(characterData.name);
-    });
+if (process.argv.length > 2) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+  request(url, (err, res, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const movie = JSON.parse(body);
+      const namespromises = movie.characters.map(char =>
+        new Promise((resolve, reject) => {
+          request(char, (err, res, body) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(JSON.parse(body).name);
+            }
+          });
+        })
+      );
+      Promise.all(namespromises).then(names => console.log(names.join('\n')));
+    }
   });
-});
+}
